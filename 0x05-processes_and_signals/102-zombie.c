c3550027a53dc1ab5dbae8860238f89641611792
sheriff-gaye <sheriffgaye5@gmail.com>
2022-07-22 17:46:58 +0000

    process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,11 +9,13 @@
  *
  * Return: Always 0.
  */
-int infinite_while(void) {
-  while (1) {
-    sleep(1);
-  }
-  return (0);
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -21,21 +23,25 @@ int infinite_while(void) {
  *
  * Return: Always 0.
  */
-int main(void) {
-  pid_t pid;
-  char count = 0;
+int main(void)
+{
+	pid_t pid;
+	char count = 0;
 
-  while (count < 5) {
-    pid = fork();
-    if (pid > 0) {
-      printf("Zombie process created, PID: %d\n", pid);
-      sleep(1);
-      count++;
-    } else
-      exit(0);
-  }
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
 
-  infinite_while();
+	infinite_while();
 
-  return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
